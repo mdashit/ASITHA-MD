18cf071c1705046c97971f636375dd9fcb21b5a1
mdashit <botashit34@gmail.com>
2024-10-18 19:29:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=a7p3EChQ#3JshS982pjp0tmuO-ojVWzIFMFZCUfuZtV3KsJOUJV4",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=u24CwTiZ#VSrwJKf7_B4N8AKrBTYgzZ5nq3rg4yaTHD7gtF4kzds",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
